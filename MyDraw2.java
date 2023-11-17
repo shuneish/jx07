47da0c4e0694b574d893df7aad27a0d0441a06fc
shuneish <grothendieck57@icloud.com>
2023-11-17 15:18:01 +0900

    aaa

--- MyDraw2.java
+++ MyDraw2.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -9,26 +8,56 @@ import java.util.List;
 
 public class MyDraw2 extends JFrame {
 
-    private DrawPanel drawPanel;
+    private JPanel drawingPanel;
+    private List<Shape> shapes; // 描画の履歴を保存するリスト
+    private int shapeToDraw; // 1: 直線, 2: 四角形, 3: 楕円
+    private Color drawColor;
 
     public MyDraw2() {
-        setTitle("MyDraw2");
-        setSize(1000, 1000);
+        setTitle("Drawing Program");
+        setSize(600, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        drawPanel = new DrawPanel();
+        shapes = new ArrayList<>();
 
+        // 描画領域用のパネル
+        drawingPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                drawShapes(g);
+            }
+        };
+        drawingPanel.setBackground(Color.WHITE);
+
+        // マウスイベントのリスナーを追加
+        drawingPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                shapes.add(new Shape(shapeToDraw, drawColor, e.getPoint(), e.getPoint()));
+                drawingPanel.repaint();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                shapes.get(shapes.size() - 1).setEnd(e.getPoint());
+                drawingPanel.repaint();
+            }
+        });
+
+        // ボタン用のパネル
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(2, 4));
 
-        JButton lineButton = createButton("Line", null, "Line");
-        JButton rectButton = createButton("Rect", null, "Rect");
-        JButton ovalButton = createButton("Oval", null, "Oval");
+        // ボタンの作成と配置
+        JButton lineButton = createButton("Line", e -> shapeToDraw = 1);
+        JButton rectButton = createButton("Rect", e -> shapeToDraw = 2);
+        JButton ovalButton = createButton("Oval", e -> shapeToDraw = 3);
         JButton blackButton = createColorButton("Black", Color.BLACK);
         JButton redButton = createColorButton("Red", Color.RED);
         JButton greenButton = createColorButton("Green", Color.GREEN);
-        JButton clearButton = createButton("Clear", null, "Clear");
-        JButton quitButton = createButton("Quit", null, "Quit");
+        JButton clearButton = createButton("Clear", e -> clearDrawing());
+        JButton quitButton = createButton("Quit", e -> System.exit(0));
 
         buttonPanel.add(lineButton);
         buttonPanel.add(rectButton);
@@ -39,148 +68,80 @@ public class MyDraw2 extends JFrame {
         buttonPanel.add(clearButton);
         buttonPanel.add(quitButton);
 
+        // レイアウトの設定
         setLayout(new BorderLayout());
-        add(drawPanel, BorderLayout.CENTER);
+        add(drawingPanel, BorderLayout.CENTER);
         add(buttonPanel, BorderLayout.SOUTH);
     }
 
-    private JButton createButton(String text, Color color, String command) {
+    private JButton createButton(String text, ActionListener listener) {
         JButton button = new JButton(text);
-        if (color != null) {
-            button.setForeground(color);
-        }
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (command != null) {
-                    processCommand(command);
-                } else {
-                    drawPanel.setCurrentColor(color);
-                    drawPanel.setCurrentShape(text);
-                }
-            }
-        });
+        button.addActionListener(listener);
         return button;
     }
 
     private JButton createColorButton(String text, Color color) {
-        JButton button = new JButton(text);
-        button.setForeground(color);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                drawPanel.setCurrentColor(color);
-            }
-        });
+        JButton button = createButton(text, e -> drawColor = color);
+        button.setBackground(color);
         return button;
     }
 
-    private void processCommand(String command) {
-        switch (command) {
-            case "Clear":
-                drawPanel.clear();
-                break;
-            case "Quit":
-                System.exit(0);
-                break;
-            default:
-                break;
+    private void drawShapes(Graphics g) {
+        for (Shape shape : shapes) {
+            shape.draw(g);
+        }
+    }
+
+    private void clearDrawing() {
+        shapes.clear();
+        drawingPanel.repaint();
+    }
+
+    private static class Shape {
+        private int type; // 1: 直線, 2: 四角形, 3: 楕円
+        private Color color;
+        private Point start;
+        private Point end;
+
+        public Shape(int type, Color color, Point start, Point end) {
+            this.type = type;
+            this.color = color;
+            this.start = start;
+            this.end = end;
+        }
+
+        public void setEnd(Point end) {
+            this.end = end;
+        }
+
+        public void draw(Graphics g) {
+            g.setColor(color);
+            switch (type) {
+                case 1: // 直線
+                    g.drawLine(start.x, start.y, end.x, end.y);
+                    break;
+                case 2: // 四角形
+                    int width = Math.abs(end.x - start.x);
+                    int height = Math.abs(end.y - start.y);
+                    int x = Math.min(start.x, end.x);
+                    int y = Math.min(start.y, end.y);
+                    g.drawRect(x, y, width, height);
+                    break;
+                case 3: // 楕円
+                    width = Math.abs(end.x - start.x);
+                    height = Math.abs(end.y - start.y);
+                    x = Math.min(start.x, end.x);
+                    y = Math.min(start.y, end.y);
+                    g.drawOval(x, y, width, height);
+                    break;
+            }
         }
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new MyDraw2().setVisible(true);
-            }
+        SwingUtilities.invokeLater(() -> {
+            MyDraw2 drawingProgram = new MyDraw2();
+            drawingProgram.setVisible(true);
         });
-    }
-}
-
-class DrawPanel extends JPanel {
-
-    private List<ShapeData> shapes;
-    private Color currentColor;
-    private String currentShape;
-    private int startX, startY, endX, endY;
-
-    public DrawPanel() {
-        shapes = new ArrayList<>();
-        currentColor = Color.BLACK;
-        currentShape = "Line";
-
-        addMouseListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent evt) {
-                startX = evt.getX();
-                startY = evt.getY();
-            }
-        });
-
-        addMouseListener(new MouseAdapter() {
-            public void mouseReleased(MouseEvent evt) {
-                endX = evt.getX();
-                endY = evt.getY();
-                saveShape();
-                repaint();
-            }
-        });
-    }
-
-    private void saveShape() {
-        shapes.add(new ShapeData(startX, startY, endX, endY, currentColor, currentShape));
-    }
-
-    public void setCurrentColor(Color color) {
-        currentColor = color;
-    }
-
-    public void setCurrentShape(String shape) {
-        currentShape = shape;
-    }
-
-    public void clear() {
-        shapes.clear();
-        repaint();
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        for (ShapeData shapeData : shapes) {
-            g.setColor(shapeData.color);
-
-            switch (shapeData.shape) {
-                case "Line":
-                    g.drawLine(shapeData.startX, shapeData.startY, shapeData.endX, shapeData.endY);
-                    break;
-                case "Rect":
-                    g.drawRect(Math.min(shapeData.startX, shapeData.endX), Math.min(shapeData.startY, shapeData.endY),
-                            Math.abs(shapeData.endX - shapeData.startX), Math.abs(shapeData.endY - shapeData.startY));
-                    break;
-                case "Oval":
-                    g.drawOval(Math.min(shapeData.startX, shapeData.endX), Math.min(shapeData.startY, shapeData.endY),
-                            Math.abs(shapeData.endX - shapeData.startX), Math.abs(shapeData.endY - shapeData.startY));
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    private static class ShapeData {
-        int startX, startY, endX, endY;
-        Color color;
-        String shape;
-
-        ShapeData(int startX, int startY, int endX, int endY, Color color, String shape) {
-            this.startX = startX;
-            this.startY = startY;
-            this.endX = endX;
-            this.endY = endY;
-            this.color = color;
-            this.shape = shape;
-        }
     }
 }
